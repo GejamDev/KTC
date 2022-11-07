db2e2cf366efcf947f1b0171354d691ce909946e
GEJAM <101797360+GejamDev@users.noreply.github.com>
2022-11-07 21:57:13 +0900

    i wanna commit die

--- Assets/Script/PixelateSprite.cs
+++ Assets/Script/PixelateSprite.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PixelateSprite : MonoBehaviour
 {
-    public SpriteRenderer sr;
-    public Material mat;
+    SpriteRenderer sr;
     const string materialName = "Own";
+    public float threshold;
+    public float pixelateAmount;
+
 
     private void Awake()
     {
@@ -16,7 +19,7 @@ public class PixelateSprite : MonoBehaviour
     public void SetMaterial()
     {
         sr = GetComponent<SpriteRenderer>();
-
+        Material mat;
         mat = Instantiate(sr.sharedMaterial);
         mat.name = materialName;
         sr.sharedMaterial = mat;
@@ -24,5 +27,11 @@ public class PixelateSprite : MonoBehaviour
     private void Update()
     {
         sr.sharedMaterial.SetFloat("_Rotation", transform.eulerAngles.z);
+        sr.sharedMaterial.SetFloat("_PosX", transform.position.x);
+        sr.sharedMaterial.SetFloat("_PosY", transform.position.y);
+        sr.sharedMaterial.SetFloat("_ScaleX", transform.lossyScale.x);
+        sr.sharedMaterial.SetFloat("_ScaleY", transform.lossyScale.y);
+        sr.sharedMaterial.SetFloat("_Threshold", threshold);
+        sr.sharedMaterial.SetFloat("_Pixelate", pixelateAmount);
     }
 }
