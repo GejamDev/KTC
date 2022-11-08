72d876f69c9b27d036984e3de1e0614570acd741
GEJAM <101797360+GejamDev@users.noreply.github.com>
2022-11-08 19:00:32 +0900

    leather club

--- Assets/Script/TESTINGSCRIPT.cs
+++ Assets/Script/TESTINGSCRIPT.cs
@@ -2,10 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//this script is used for testing various stuff so yeah 
+[ExecuteInEditMode]
 public class TESTINGSCRIPT : MonoBehaviour
 {
-    //this script is used for testing various stuff so yeah 
-    
+
+    public SpriteRenderer sr;
+    private void Update()
+    {
+        Debug.Log(sr.sprite.associatedAlphaSplitTexture);
+    }
 }
 
-//deep dark fantazies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//deep dark fant  ass  ies
